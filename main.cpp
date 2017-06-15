bb9e5c9557cc55517d8ba517d3b0bd3fa07769b7
asand017 <asand017@ucr.edu>
2017-06-15 15:53:53 -0700

    fixed seg fault/miscellanous in main.cpp

--- main.cpp
+++ main.cpp
@@ -26,16 +26,16 @@ facility_set buttons[2];
 
 //event_set going_up[2];
 //event_set going_dn[2];
-event_set get_off_now[2]; 
-event_set hop_on[2];
+event_set get_off_now("get off now", 2); 
+event_set hop_on("hop on", 2);
 
-event_set boarded[2]; 
+event_set boarded("boarded", 2); 
 
 //Wakes up elevators
 event Wakeup("Wakeup");
 
-event_set Coming_up[9];
-event_set Coming_dn[9];
+event_set Coming_up("up", 9);
+event_set Coming_dn("down", 9);
 
 int want_up[9];
 int want_dn[9];
@@ -117,7 +117,7 @@ void make_passengers(long whereami)
   while(clock < 1440)       
   {
     hold(expntl(10)); 
-    long group = group_size();
+    long group = 100;//group_size();
     for (long i=0;i<group;i++) 
       passenger(whereami); 
   }
@@ -128,16 +128,16 @@ void make_passengers(long whereami)
 
 void passenger(long whoami)
 {
-
   //whoami --> the current floor of the passenger
   
-  const char* myName=people[whoami].c_str();
+  
+  const char* myName=people[whoami % 3].c_str();
   create(myName);
-
+  
   // Give random destination floor
 
-  int dest_floor;
-
+  int dest_floor = 0;
+  
   //cout << want_up[0] << " " << want_dn[0] << endl;
 
   while(dest_floor == whoami){
@@ -162,10 +162,10 @@ void passenger(long whoami)
 
   //wait for announcemnt of which elevator is coming
   if(pass_dir ==1){
-    (*Coming_up)[whoami].wait();
+    Coming_up[whoami].wait();
   }
   else if(pass_dir == 2){
-    (*Coming_dn)[whoami].wait();
+    Coming_dn[whoami].wait();
   }
 
   //elv_num is elevator that came to floor
@@ -174,17 +174,17 @@ void passenger(long whoami)
   //while(elevs[elv_num].direction != pass_dir);
  
   if(dest_floor > whoami){
-    want_up[whoami]--;
+    want_up[whoami] -= 1;
   }
   else if(dest_floor < whoami){
-    want_dn[whoami]--;
+    want_dn[whoami] -= 1;
   }
   
 
   hold(uniform(0.5,1.0));        // takes time to get seated
   //boarded.set();                 // tell driver you are in your seat
   //(*buttons) [whoami].release();     // let next person (if any) access button
-  get_off_now->wait_any();            // everybody off when shuttle reaches next stop*/
+  //get_off_now->wait_any();            // everybody off when shuttle reaches next stop*/
   
   
 }
@@ -206,9 +206,9 @@ void elevator(int elevatornum) {
 		int goto_floor;
     int dn_visit = -1;
     int up_visit = 20;  
-    for(int i = 0; i < 10; i++){
-	     if(want_up[i] > 0 && i < up_visit && (*Coming_up)[i].state() == NOT_OCC) up_visit = i;
-	     if(want_dn[i] > 0 && i > dn_visit && (*Coming_dn)[i].state() == NOT_OCC) dn_visit = i;
+    for(int i = 0; i < 9; i++){
+	     if(want_up[i] > 0 && i < up_visit && Coming_up[i].state() == NOT_OCC) up_visit = i;
+	     if(want_dn[i] > 0 && i > dn_visit && Coming_dn[i].state() == NOT_OCC) dn_visit = i;
     }
     
     bool dn = false, up = false, both = false;
@@ -218,7 +218,7 @@ void elevator(int elevatornum) {
     
     //Compare floor with maximum number of visits for up and down
     //Choose whichever is closest
-    int min_dn_visit = 100, min_up_visit = 100, min_elevator_up = -1, min_elevatord_dn = -1;
+    int min_dn_visit = 100, min_up_visit = 100, min_elevator_up = -1, min_elevator_dn = -1;
     for(int i = 0; i < elevs.size(); i++){
         int distance_up_visit = 101, distance_dn_visit = 101;
         if(elevs[elevatornum].direction == 0){
@@ -245,19 +245,18 @@ void elevator(int elevatornum) {
     
     if(min_elevator_up == elevatornum){  
       goto_floor = up_visit;
-      elevs[elevatornum].next_floor = goto_floor;
+      elevs[elevatornum].next_stop = goto_floor;
     }
-    
-    elese if(min_elevator_dn == elevatornum){
-      goto_floor = dn_visit
-      elevs[elevatornum].next_floor = goto_floor;
+    else if(min_elevator_dn == elevatornum){
+      goto_floor = dn_visit;
+      elevs[elevatornum].next_stop = goto_floor;
     }
     
     
     //long who_pushed = elevator_called->wait_any();
     //(*elevator_called) [who_pushed].set(); // loop exit needs to see event
 
-    long seats_used = 0;              // shuttle is initially empty
+    //long seats_used = 0;              // shuttle is initially empty
     elevator_occ.note_value(seats_used);
 
     hold(5);  // 5 minutes to reach car lot stop
@@ -337,7 +336,7 @@ void load_elevator(long whereami, long & on_board)  // manage passenger loading
   //while((on_board < NUM_SEATS) &&
     //((*buttons) [whereami].num_busy() + (*buttons) [whereami].qlength() > 0))
   //{
-    (*hop_on) [whereami].set();// invite one person to board
+    hop_on[whereami].set();// invite one person to board
     //boarded.wait();  // pause until that person is seated
     on_board++;
     hold(TINY);  // let next passenger (if any) reset the button
